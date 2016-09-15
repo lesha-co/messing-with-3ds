0272aab76d584e686333a8760b5664bd2d1f608d
malyavkin <a@malyavk.in>
2016-09-16 01:36:58 +0300

    make ui colorful, add more options

--- source/main.cpp
+++ source/main.cpp
@@ -4,27 +4,66 @@
 #include "mandel.h"
 #include <unistd.h>
 #include "metrics.h"
+#include "string_utils.h"
+
 using namespace std;
 
 
 PrintConsole bottom, top;
+size_t quantify(size_t input, size_t min, size_t max, size_t pallet_size){
+    // returns from 0 to pallet_size-1 inclusive
+    size_t range = max-min;
+    double quant_range = double(range)/pallet_size;
+    if (input <= min) return 0;
+    if (input >= max) return pallet_size-1;
+    size_t current = (size_t )(double(input)/quant_range);
 
-void run_mandel_gfx(fieldDef field, size_t max_iter, vector<bgr_pixel> pallet){
+    if(current >= pallet_size){
+        printf("\nerror, %d->%d, in(%d:%d), pallet: %d\n", input, current, min, max, pallet_size);
+
+    }
+    return current;
+}
+
+void run_pallet(vector<bgr_pixel> pallet){
+    gfxSetDoubleBuffering(GFX_TOP, false);
+    u8* fb = gfxGetFramebuffer(GFX_TOP, GFX_LEFT, NULL, NULL);
+    for (size_t i = 0; i < TOP_PIXELS; ++i) {
+        size_t quant = quantify(i, 0, TOP_PIXELS, pallet.size());
+        bgr_pixel px = pallet[quant];
+
+        fb[i*3] = px.b;
+        fb[i*3+1] = px.g;
+        fb[i*3+2] = px.r;
+    }
+}
+
+void run_mandel_gfx(fieldDef field, size_t max_iter, vector<bgr_pixel> pallet, bool useHistogram){
 
     gfxSetDoubleBuffering(GFX_TOP, false);
     double tolerance = 2.0;
     // getting pointer to lower screen frame buffer
     u8* fb = gfxGetFramebuffer(GFX_TOP, GFX_LEFT, NULL, NULL);
     vector<vector<size_t>> iters = getField(field, max_iter, tolerance);
-    histogram_acc h = histogram(iters);
-    map<size_t, size_t> mapping = buildPalletMappings(h, pallet.size());
 
+    histogram_acc h;
+    map<size_t, size_t> mapping;
+    size_t quant;
+    if(useHistogram){
+        h = histogram(iters);
+        mapping = buildPalletMappings(h, pallet.size());
+    }
     for (size_t i = 0; i < iters.size(); ++i) {
         vector<size_t> line = iters[i];
 
         for (size_t j = 0; j < line.size(); ++j) {
             size_t cell = line.at(j);
-            size_t quant = mapping[cell];
+
+            if(useHistogram){
+                quant = mapping[cell];
+            } else {
+                quant = quantify(cell, 0, max_iter, pallet.size());
+            }
             if(quant >= pallet.size()){
                 printf("FAIL %d", quant);
 
@@ -70,15 +109,19 @@ void run_mandel(fieldDef field){
 
 }
 
-void run_print_stats(fieldDef def, size_t q, vector<bgr_pixel> pallet) {
+void run_print_stats(fieldDef def, size_t q, vector<bgr_pixel> pallet, bool useHistogram) {
     consoleInit(GFX_BOTTOM, &bottom);
     consoleSelect(&bottom);
-    printf("Running mandelGFX\n");
-    printf("Im: (% .2e, % .2e)\n", def.IM_from, def.IM_to);
-    printf("Re: (% .2e, % .2e)\n", def.RE_from, def.RE_to);
+    printf(colorize(COLOR_MAGENTA, "Running mandelGFX\n").c_str());
+    printf("\n");
+    printf("%s: (% .2e, % .2e)\n", colorize(COLOR_WHITE, COLOR_BLUE, "IM").c_str(), def.IM_from, def.IM_to);
+    printf("%s: (% .2e, % .2e)\n", colorize(COLOR_WHITE, COLOR_RED, "RE").c_str(), def.RE_from, def.RE_to);
     printf("Display %dx%d\n", def.nRE, def.nIM);
     printf("Pallet size %d\n", pallet.size());
-    printf("With %d iterations\n", q);
+    printf("[A] Histogram color correction: [%s]\n", useHistogram?
+                                  colorize(COLOR_WHITE,COLOR_GREEN,"YES").c_str():
+                                  colorize(COLOR_WHITE,COLOR_RED,"NO").c_str());
+    printf("[Y] Max iterations: %d\n", q);
 
 }
 
@@ -109,10 +152,12 @@ int main(int argc, char **argv)
     size_t Q_DRAFT_MAX_ITER = 50;
     size_t Q_BEST_MAX_ITER = 500;
     size_t CURRENT_Q = Q_DRAFT_MAX_ITER;
-
-    run_print_stats(field, CURRENT_Q, pallet);
-    run_mandel_gfx(field, CURRENT_Q, pallet);
-    printf("Ready.\n");
+    bool useHistogram = true;
+    run_pallet(pallet);
+    printf(colorize(COLOR_RED, "Now starting...\n").c_str());
+    usleep(1000000);
+    run_print_stats(field, CURRENT_Q, pallet, useHistogram);
+    run_mandel_gfx(field, CURRENT_Q, pallet, useHistogram);
     ///
     /// run once end
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -178,10 +223,13 @@ int main(int argc, char **argv)
             if (kDown & KEY_Y) {
                 CURRENT_Q = (CURRENT_Q == Q_DRAFT_MAX_ITER? Q_BEST_MAX_ITER:Q_DRAFT_MAX_ITER);
             }
+            if (kDown & KEY_A) {
+                useHistogram = !useHistogram;
+            }
 
-            run_print_stats(field, CURRENT_Q, pallet);
-            run_mandel_gfx(field, CURRENT_Q, pallet);
-            printf("Ready.\n");
+            run_print_stats(field, CURRENT_Q, pallet, useHistogram);
+            run_mandel_gfx(field, CURRENT_Q, pallet, useHistogram);
+            printf(colorize(COLOR_CYAN, "Ready").c_str());
         }
         ///
         /// run loop end
